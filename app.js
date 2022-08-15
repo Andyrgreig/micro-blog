48d056b39069529805c9153db7c283cef6dfd11c
andygreig <andyrgreig@gmail.com>
2022-08-15 09:56:38 +0100

    first-commit

---
+++ app.js
@@ -0,0 +1,57 @@
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const favicon = require("serve-favicon");
+const dotenv = require("dotenv").config();
+
+//require routes
+const blogRouter = require("./routes/blogRoutes");
+const userRouter = require("./routes/userRoutes");
+
+const app = express();
+
+//Set up mongoose connection
+const mongoose = require("mongoose");
+const mongoDB = process.env.MONGODB;
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+app.use(favicon(__dirname + "/favicon.ico"));
+
+// app.use("/product", productRouter);
+// app.use("/users", usersRoutes);
+app.get("/", (req, res) => {
+  res.status(200).render("index", {
+    title: "Log in",
+  });
+});
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,7 @@
+const dotenv = require("dotenv").config();
+const app = require("./app");
+
+const port = process.env.PORT;
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
